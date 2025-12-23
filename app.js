dcf88ad5a327779e169da0a0772f2e361f4870cd
lolkekfe <72572991+lolkekfe@users.noreply.github.com>
2025-12-23 19:22:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-/* ===== FIREBASE INIT ===== */
-const firebaseConfig = {
-  apiKey: "AIzaSyCGqNn_jx13SKHBYMZLhUJ7nEbK32vAJx4",
-  authDomain: "curatorterminal.firebaseapp.com",
-  databaseURL: "https://curatorterminal-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "curatorterminal",
-  storageBucket: "curatorterminal.appspot.com",
-  messagingSenderId: "577721444184",
-  appId: "1:577721444184:web:58f4d0aeede9a8c0ba672d",
-  measurementId: "G-X0N5JT0K8D"
-};
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
 /* ===== AUTH SYSTEM ===== */
 const HASH_ADMIN   = "10cda"; // EOD
 const HASH_CURATOR = "be32";  // 123
---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCGqNn_jx13SKHBYMZLhUJ7nEbK32vAJx4",
+  authDomain: "curatorterminal.firebaseapp.com",
+  databaseURL: "https://curatorterminal-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "curatorterminal",
+  storageBucket: "curatorterminal.appspot.com",
+  messagingSenderId: "577721444184",
+  appId: "1:577721444184:web:58f4d0aeede9a8c0ba672d",
+  measurementId: "G-X0N5JT0K8D"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
